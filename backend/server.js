111d9b81cd4084fc6adf4f022db7e0aa6d27cb80
Kunal Prashant <kunalprashant77@gmail.com>
2024-06-30 19:07:42 +0530

    added files for deployment in vercel

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 dotenv.config();
 connectDB();
 
-const coffeeShopRoutes = require('./routes/coffeeShopRoutes');
-const productRoutes = require('./routes/productRoutes');
+const coffeeShopRoutes = require("./routes/coffeeShopRoutes");
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["https://coffee-shop-finder-3prl.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+app.use(express.json());
 
-
-app.use('/api/shops', coffeeShopRoutes);
-app.use('/api/products', productRoutes);
-
+app.use("/api/shops", coffeeShopRoutes);
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 3000;
 
--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,36 @@
 import axios from 'axios';
 
-const API_URL = process.env.API_URL;
+const API_URL = "https://coffee-shop-finderbr.vercel.app/api/";
 
-export const getShops = () => axios.get(`${API_URL}/shops`);
-export const getShopById = (id) => axios.get(`${API_URL}/shops/${id}`);
-export const getProductsByShop = (shopId) => axios.get(`${API_URL}/products/${shopId}/products`);
+export const getShops = () => {
+  return axios.get(`${API_URL}/shops`)
+    .then(response => {
+      return response.data; // Assuming you want to return data from the response
+    })
+    .catch(error => {
+      console.error('Error fetching shops:', error);
+      throw error; // Rethrow or handle the error as needed
+    });
+};
+
+export const getShopById = (id) => {
+  return axios.get(`${API_URL}/shops/${id}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      console.error(`Error fetching shop with ID ${id}:`, error);
+      throw error;
+    });
+};
+
+export const getProductsByShop = (shopId) => {
+  return axios.get(`${API_URL}/products/${shopId}/products`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      console.error(`Error fetching products for shop ID ${shopId}:`, error);
+      throw error;
+    });
+};
